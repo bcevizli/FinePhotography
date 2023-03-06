0eac782b0f01723ec267ae98fd8aa14eb2daff78
burak cevizli <burak.cevizli7@gmail.com>
2023-03-06 21:42:06 +0300

    UI Changes

--- FinePhotography/Views/PhotoCell.swift
+++ FinePhotography/Views/PhotoCell.swift
@@ -22,7 +22,7 @@ class PhotoCell: UITableViewCell {
         let v = UILabel()
         v.translatesAutoresizingMaskIntoConstraints = false
         v.font = v.font.withSize(16)
-        v.textColor = .red
+        v.textColor = .white
         return v
     }()
     
@@ -30,7 +30,7 @@ class PhotoCell: UITableViewCell {
         let v = UILabel()
         v.translatesAutoresizingMaskIntoConstraints = false
         v.font = v.font.withSize(14)
-        v.textColor = .blue
+        v.textColor = .white
         return v
     }()
     
